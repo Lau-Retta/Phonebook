b6fd4336d4590668f0c66768de93f43c1dd18664
Lautaro Retta <lautaroretta@gmail.com>
2024-02-18 02:39:03 -0300

    exercise part3 (3.10)-1

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ let persons =[
   })
 
 
-const PORT = 3001
-app.listen(PORT, () => {
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
